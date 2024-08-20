54261adf51d81dd0c529029ebe25db65588742b9
Jose Salinas <jascjasc081@gmail.com>
2024-08-20 16:38:54 -0600

    paginajose

---
+++ view/username/paginajose.php
@@ -0,0 +1,52 @@
+<?php
+
+
+require_once(__DIR__ . "/../head/head.php");
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="tabla.css">
+    <title>Document</title>
+</head>
+<body>
+<div>
+    <br>
+    <br>
+    <br>
+    <br>
+        <table color>
+            <tr>
+                <th>Nombre:</th>
+                <td>Jose Alfredo Salinas Caudillo</td>
+                
+            </tr>
+            <tr>
+                <th>Edad:</th>
+                <td>21 Años</td>
+            </tr>
+            <tr>
+                <th>Telefono:</th>
+                <td>9212640616</td>
+            </tr>
+            <tr>
+                <th>Correo:</th>
+                <td>jascjasc08@gmail.com</td>
+            </tr>
+            <tr>
+                <th>Grado de estudios:</th>
+                <td>Universitario</td>
+            </tr>
+            <tr>
+                <th>Habilidades:</th>
+                <td>Programacion web frontend</td>
+            </tr>      
+        </table>
+    </div>
+
+</body>
+</html>
