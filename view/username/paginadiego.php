9430b9def756bc482b67208290d5f72a899563fd
Da_Internet <diego-canto@hotmail.com>
2024-08-20 16:36:41 -0600

    Modificacion de Pagina Diego Canto

---
+++ view/username/paginadiego.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Proyecto 3er Parcial</title>
+</head>
+<body>
+    <h1>
+        Pagina de Diego Alberto Canto Ricardez
+    </h1>
+
+    <h2>
+        Fecha: 20/02/2024
+    </h2>
+    <p>
+        Edad: 21
+    </p>
+    <br>
+    <p>
+        Universidad Tecnologica del Sureste de Veracruz
+    </p>
+    <br>
+    <p>
+        Trabajo: Estudiante
+    </p>
+    <br>
+    <p>
+        Contacto: 921 118 0790 | dsm20190062.dcanto@alumnos.utsv.edu.mx
+    </p>
+    <br>
+    <p>
+        Habilidades: HTML, CSS, PHP, JS, JAVA, SQL
+    </p>
+    <br>
+
+</body>
+</html>
